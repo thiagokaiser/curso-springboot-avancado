797f09b22777297f7c7e5c789224674990790bc7
unknown <thiago.kaiser@hotmail.com>
2020-05-27 11:51:37 -0300

    first commit

---
+++ src/test/java/com/kaiser/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiser.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
