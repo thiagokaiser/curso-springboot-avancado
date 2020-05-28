49c4ee547cf4ecde3816e5a35131a5c30cc33a88
unknown <thiago.kaiser@hotmail.com>
2020-05-28 10:29:24 -0300

    Primeira classe de dominio - Categoria

--- src/main/java/com/kaiser/cursomc/CursomcApplication.java
+++ src/main/java/com/kaiser/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package com.kaiser.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.kaiser.cursomc.domain.Categoria;
+import com.kaiser.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {		
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));		
+		
 	}
 
 }
---
+++ src/main/java/com/kaiser/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.kaiser.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kaiser.cursomc.domain.Categoria;
+import com.kaiser.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
