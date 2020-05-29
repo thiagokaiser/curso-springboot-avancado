b3909373dff120a6b4d810dc5a15d0d046e31427
unknown <thiago.kaiser@hotmail.com>
2020-05-29 10:29:17 -0300

    Paginacao com parametros opcionais - Categoria

--- src/main/java/com/kaiser/cursomc/CursomcApplication.java
+++ src/main/java/com/kaiser/cursomc/CursomcApplication.java
@@ -63,6 +63,12 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Categ 03");
+		Categoria cat4 = new Categoria(null, "Categ 04");
+		Categoria cat5 = new Categoria(null, "Categ 05");
+		Categoria cat6 = new Categoria(null, "Categ 06");
+		Categoria cat7 = new Categoria(null, "Categ 07");
+		Categoria cat8 = new Categoria(null, "Categ 08");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -75,7 +81,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));		
 		
-		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepo.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8));
 		produtoRepo.saveAll(Arrays.asList(p1,p2,p3));
 		
 		Estado est1 = new Estado(null, "Santa Catarina");
--- src/main/java/com/kaiser/cursomc/services/CategoriaService.java
+++ src/main/java/com/kaiser/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.kaiser.cursomc.domain.Categoria;
@@ -47,5 +50,9 @@ public class CategoriaService {
 		return repo.findAll();
 	}
 	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);		
+	}	
 	
 }
