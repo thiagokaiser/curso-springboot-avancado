0eb5634cd4ef5ad826676aab24f7dee6eb118830
unknown <thiago.kaiser@hotmail.com>
2020-06-04 16:10:31 -0300

    Documentacao com Swagger

--- src/main/java/com/kaiser/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/kaiser/cursomc/resources/CategoriaResource.java
@@ -22,6 +22,10 @@ import com.kaiser.cursomc.domain.Categoria;
 import com.kaiser.cursomc.dto.CategoriaDTO;
 import com.kaiser.cursomc.services.CategoriaService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
@@ -29,6 +33,7 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;	
 	
+	@ApiOperation(value = "Busca por id")
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		
@@ -37,6 +42,7 @@ public class CategoriaResource {
 		
 	}
 	
+	@ApiOperation(value = "Insere categoria")
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto){		
@@ -48,6 +54,7 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@ApiOperation(value = "Atualiza categoria")
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id){
@@ -57,6 +64,10 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@ApiOperation(value = "Remove categoria")
+	@ApiResponses(value = {
+			@ApiResponse(code = 400, message = "Não é possível excluir uma categoria que possui produtos"),
+			@ApiResponse(code = 404, message = "Código inexistente") })
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
@@ -66,6 +77,7 @@ public class CategoriaResource {
 		
 	}
 	
+	@ApiOperation(value = "Retorna todas categorias")
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		
@@ -75,6 +87,7 @@ public class CategoriaResource {
 		
 	}
 	
+	@ApiOperation(value = "Retorna todas categorias com paginação")
 	@RequestMapping(value="/page", method=RequestMethod.GET)
 	public ResponseEntity<Page<CategoriaDTO>> findPage(
 			@RequestParam(value="page", defaultValue = "0") Integer page, 
