ac2c880ccff6557edcfd7f4a7c4dbaef78ea85fa
unknown <thiago.kaiser@hotmail.com>
2020-06-05 14:50:38 -0300

    Configurando Amazon S3

--- src/main/java/com/kaiser/cursomc/CursomcApplication.java
+++ src/main/java/com/kaiser/cursomc/CursomcApplication.java
@@ -1,19 +1,24 @@
 package com.kaiser.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.kaiser.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {	
+	
+	@Autowired
+	private S3Service s3service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {		
-		
+	public void run(String... args) throws Exception {
+		s3service.uploadFile("C:\\temp\\perfil.jpg");
 	}
-
 }
