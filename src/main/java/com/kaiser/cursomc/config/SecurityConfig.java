0d65f1b94983499b6ad833f286ab04329479e1ef
unknown <thiago.kaiser@hotmail.com>
2020-06-06 12:42:31 -0300

    Salvando imagem de Perfil

--- src/main/java/com/kaiser/cursomc/config/SecurityConfig.java
+++ src/main/java/com/kaiser/cursomc/config/SecurityConfig.java
@@ -49,8 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	};
 
 	private static final String[] PUBLIC_MATCHERS_POST = {
-			"/clientes",
-			"/clientes/picture",
+			"/clientes",			
 			"/auth/forgot/**"
 	};
 	
--- src/main/java/com/kaiser/cursomc/services/ClienteService.java
+++ src/main/java/com/kaiser/cursomc/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.kaiser.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -42,6 +44,15 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 	
 	public Cliente find(Integer id) {
 		
@@ -111,7 +122,18 @@ public class ClienteService {
 		newObj.setEmail(obj.getEmail());		
 	}
 	
-	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3Service.uploadFile(multipartFile);
+	public URI uploadProfilePicture(MultipartFile multipartFile) {		
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
+		String fileName = prefix + user.getId() + ".jpg";
+				
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}	
 }
